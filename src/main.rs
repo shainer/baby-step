400332b07bf532eb1b46fe988b02f990b090840c
shainer <syn.shainer@gmail.com>
2017-05-22 23:14:35 +0100

    Some progress: algorithm works but we are hardcoding one value.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
 use std::collections::HashMap;
 
 fn main() {
-    let p = 31;
-    let g = 3;
+    // TODO: input parameters from command line.
+    let p : u32 = 31;
+    let g : u32 = 3;
     let h = 6;
 
     // TODO: verify the parameters and print them
     println!("My generator is {}", g);
 
-    let m = (p as f64).sqrt().ceil() as i32;
+    let m = (p as f64).sqrt().ceil() as u32;
+    println!("M is {}", m);
     let mut precomp = HashMap::new();
 
     for j in 0..m {
-        precomp.insert(j, g.pow(j))
+        precomp.insert((g.pow(j) as u32), j);
     }
 
-    let y = g.pow(-m);
+    // This should be an invmod, g^-m.
+    let y : u32 = 2;
+    let mut start : u32 = h;
+    let mut found = false;
+
+    for i in 0..m {
+        if precomp.contains_key(&start) {
+            match precomp.get(&start) {
+                Some(value) => println!("Found discrete log: {}", (i * m) + value),
+                // TODO: raise an error when this happens.
+                None => println!("Unrecoverable error!!!")
+            }
+
+            found = true;
+            break;
+        }
+
+        start = start * y % p;
+    }
+
+    if !found {
+        println!("Error: discrete logarithm not found.")
+    }
 }
